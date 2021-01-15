5b2dacbdf3ab80373e191e58bdddb50db0d3251f
osamaalkarmy <74108150+osamaalkarmy@users.noreply.github.com>
2021-01-15 16:50:27 +0200

    Add files via upload

---
+++ php.php
@@ -0,0 +1,25 @@
+<title>Osama Alkarmi PHP</title>
+<h1>Osama Alkarmi PHP</h1>
+<?php
+print "The SHA256 hash of \"Osama Alkarmi\" is ";
+print hash('sha256', 'Osama Alkarmi');
+$name = ' ASCII ART:
+
+       __  
+      / __ \ 
+     | |  | |
+     | |  | |
+     | |__| |
+      \__/ 
+         
+         
+                                                                                                     
+';
+
+
+echo "<pre>";
+echo $name;
+echo "</pre>";
+
+
+?>
